b47c9a00dbe483232b47589c51f0c44cbb4a4cd8
Grace <kalinina.grace@gmail.com>
2022-11-26 22:59:07 +0200

    Changes

---
+++ Sigurnost/Sigurnost-WebApp/Controllers/PoliciesTypeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sigurnost_WebApp.Controllers
+{
+    public class PoliciesTypeController : Controller
+    {
+        public IActionResult PoliciesType()
+        {
+            return View();
+        }
+    }
+}
